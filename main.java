80f01e3ad610ed3f2df82dddaa23da57ba8ee32e
mahiachowdhury1 <144488566+mahiachowdhury1@users.noreply.github.com>
2023-12-01 15:30:17 -0600

    Add files via upload

---
+++ main.java
@@ -0,0 +1,39 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+       package chat.example;
+
+        public class phase03 {
+            private boolean editUserProfile;
+
+            public editUserID() {
+                loginID = true;
+                loginID = false;
+            }
+
+            public void editPassword() {
+                userPassword = true;
+            }
+
+            public void editPassword() {
+                userPassword = false;
+            }
+
+            public void editEmail() {
+                userEmail = true;
+            }
+
+            public void editEmail() {
+                userEmail = false;
+            }
+
+            public boolean editUserProfile() {
+                return verificationModification;
+            }
+
+        }
+    }
+
+}
+
---
+++ phase03.java
@@ -0,0 +1,32 @@
+public class phase03 {
+
+    package chat.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+    class phase03 {
+
+        editUserProfile = verificationModification();
+
+        @Test
+        void editUserProfile() {
+        }
+
+        @Test
+        void editPassword() {
+            assertTrue(editUserProfile.Enabled());
+            assertFalse(editUserProfile.Enabled());
+        }
+        @Test
+        void editEmail() {
+            assertTrue(editEmail.check());
+            assertFalse(editEmail.check());
+        }
+
+    }
+
+}
